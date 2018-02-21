3585ed9dc7de4106152188988800a0877dcf5127
Rebecca Skinner <traybaby@gmail.com>
2018-02-21 19:07:39 +0800

    My solution for the Feb meetup katas

--- kata-night-21-2-18/list_kata/lib/list_kata.ex
+++ kata-night-21-2-18/list_kata/lib/list_kata.ex
@@ -7,33 +7,44 @@ defmodule ListKata do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
+  def count(l), do: do_count(l, 0)
+  defp do_count([], count), do: count
+  defp do_count([_ | rest], count), do: do_count(rest, count+1)
 
-  end
+  @spec reverse(list) :: list
+  def reverse([]), do: []
+  def reverse(list), do: do_reverse(list, [])
+
+  defp do_reverse([], stuff), do: stuff
+  defp do_reverse([head | rest], stuff), do: do_reverse(rest, [head | stuff])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-
-  end
+  def map(l, f), do: do_map(l, [], f)
+  defp do_map([], stuff, _), do: reverse(stuff)
+  defp do_map([head | rest], stuff, func), do: do_map(rest, [func.(head) | stuff], func)
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-
+  def filter(l, f), do: do_filter(l, [], f)
+  defp do_filter([], stuff, _), do: reverse(stuff)
+  defp do_filter([head | rest], stuff, func) do
+    if func.(head) do
+      do_filter(rest, [head | stuff], func)
+    else
+      do_filter(rest, stuff, func)
+    end
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-
-  end
+  def reduce([], acc, _), do: acc
+  def reduce([head | rest], acc, f), do: reduce(rest, f.(head, acc), f)
 
   @spec append(list, list) :: list
-  def append(a, b) do
-
-  end
+  def append(a, b), do: do_append(reverse(a), b)
+  defp do_append([], stuff), do: stuff
+  defp do_append([head | rest], stuff), do: do_append(rest, [head | stuff])
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-
-  end
+  def concat([]), do: []
+  def concat([head | rest]), do: append(head, concat(rest))
 end
